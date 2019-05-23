575908321450269ff3c7a9b7f93addb3adaae394
Richard Nelson <unixabg@gmail.com>
2019-05-23 09:42:50 -0500

    Isolate paths with _BASEDIR and _BUILDDIR variables in stage-1.sh.

--- stage-1.sh
+++ stage-1.sh
@@ -18,7 +18,7 @@ _VER="2.0-beta"
 
 dropbearpi_check(){
 	# Test for authorized_keys file
-	if [ ! -f ${_BASEDIR}/../authorized_keys ]; then
+	if [ ! -f ${_BASEDIR}/authorized_keys ]; then
 		echo "Dropbear authorized_keys file missing. Exiting..."
 		exit 1
 	fi
@@ -30,15 +30,15 @@ dropbearpi(){
 	# Put authorized keys where they go
 	echo "Attempting dropbearpi..."
 
-	chroot ${_BASEDIR}/root apt-get -y install dropbear
+	chroot ${_BUILDDIR}/root apt-get -y install dropbear
 
-	mkdir -p ${_BASEDIR}/root/root/.ssh/
-	cat ${_BASEDIR}/../authorized_keys > ${_BASEDIR}/root/etc/dropbear-initramfs/authorized_keys
-	cat ${_BASEDIR}/../authorized_keys > ${_BASEDIR}/root/root/.ssh/authorized_keys
+	mkdir -p ${_BUILDDIR}/root/root/.ssh/
+	cat ${_BASEDIR}/authorized_keys > ${_BUILDDIR}/root/etc/dropbear-initramfs/authorized_keys
+	cat ${_BASEDIR}/authorized_keys > ${_BUILDDIR}/root/root/.ssh/authorized_keys
 	# Update dropbear for some sleep in initramfs
-	sed -i 's/run_dropbear &/sleep 5\nrun_dropbear &/g' ${_BASEDIR}/root/usr/share/initramfs-tools/scripts/init-premount/dropbear
+	sed -i 's/run_dropbear &/sleep 5\nrun_dropbear &/g' ${_BUILDDIR}/root/usr/share/initramfs-tools/scripts/init-premount/dropbear
 	# Change the port that dropbear runs on to make our lives easier
-	sed -i 's/#DROPBEAR_OPTIONS=/DROPBEAR_OPTIONS="-p 2222"/g' ${_BASEDIR}/root/etc/dropbear-initramfs/config
+	sed -i 's/#DROPBEAR_OPTIONS=/DROPBEAR_OPTIONS="-p 2222"/g' ${_BUILDDIR}/root/etc/dropbear-initramfs/config
 
 	echo "...dropbearpi call completed!"
 }
@@ -61,71 +61,71 @@ encryptpi(){
 	# Download arm image if we don't already have it
 	_IMAGE=https://images.offensive-security.com/arm-images/kali-linux-2019.1-rpi3-nexmon-64.img.xz
 	_IMAGENAME=`basename ${_IMAGE}`
-	if [ -f ${_BASEDIR}/../${_IMAGENAME} ]; then
+	if [ -f ${_BASEDIR}/${_IMAGENAME} ]; then
 		echo "Awesome, ARM image ${_IMAGENAME} already exists. Skipping Download"
 	else
 		echo "Downloading ARM image from $image"
-		wget ${_IMAGE} -O ${_BASEDIR}/../${_IMAGENAME}
+		wget ${_IMAGE} -O ${_BASEDIR}/${_IMAGENAME}
 	fi
 
 	# Extract files from image
-	mkdir ${_BASEDIR}/root
-	mkdir ${_BASEDIR}/mount
+	mkdir ${_BUILDDIR}/root
+	mkdir ${_BUILDDIR}/mount
 	echo "Extracting image: ${_IMAGENAME}"
-	xz --decompress --stdout ../${_IMAGENAME} > ${_BASEDIR}/kali.img
+	xz --decompress --stdout${_BASEDIR}/${_IMAGENAME} > ${_BUILDDIR}/kali.img
 	echo "Mounting loopback"
-	loopdev=`losetup -f --show ${_BASEDIR}/kali.img`
+	loopdev=`losetup -f --show ${_BUILDDIR}/kali.img`
 	partprobe ${loopdev}
 	# Extract root partition
-	mount ${loopdev}p2 ${_BASEDIR}/mount
+	mount ${loopdev}p2 ${_BUILDDIR}/mount
 	echo "Syncing /root"
-	rsync -HPavz -q ${_BASEDIR}/mount/ ${_BASEDIR}/root/
-	umount ${_BASEDIR}/mount
+	rsync -HPavz -q ${_BUILDDIR}/mount/ ${_BUILDDIR}/root/
+	umount ${_BUILDDIR}/mount
 	# Extract boot partition
-	mount ${loopdev}p1 ${_BASEDIR}/mount/
+	mount ${loopdev}p1 ${_BUILDDIR}/mount/
 	echo "Syncing /root/boot"
-	rsync -HPavz -q ${_BASEDIR}/mount/ ${_BASEDIR}/root/boot/
-	umount ${_BASEDIR}/mount
+	rsync -HPavz -q ${_BUILDDIR}/mount/ ${_BUILDDIR}/root/boot/
+	umount ${_BUILDDIR}/mount
 	# Clear loopback
-	rmdir ${_BASEDIR}/mount
+	rmdir ${_BUILDDIR}/mount
 	echo "Cleaning loopback"
 	losetup -d ${loopdev}
-	rm ${_BASEDIR}/kali.img
+	rm ${_BUILDDIR}/kali.img
 
 	# Setup qemu emulator for aarch64
 	echo "Copying qemu emulator to chroot"
-	cp /usr/bin/qemu-aarch64-static ${_BASEDIR}/root/usr/bin/
+	cp /usr/bin/qemu-aarch64-static ${_BUILDDIR}/root/usr/bin/
 
 	# Install some extra stuff
-	chroot ${_BASEDIR}/root apt-get update
-	chroot ${_BASEDIR}/root apt-get -y install cryptsetup busybox
+	chroot ${_BUILDDIR}/root apt-get update
+	chroot ${_BUILDDIR}/root apt-get -y install cryptsetup busybox
 
 	# Tell pi to use initramfs
-	echo "initramfs initramfs.gz followkernel" >> ${_BASEDIR}/root/boot/config.txt
+	echo "initramfs initramfs.gz followkernel" >> ${_BUILDDIR}/root/boot/config.txt
 
 	##########
 	# Begin cryptsetup
 	echo "The encryptpi stage completed!"
 	# Update /boot/cmdline.txt to boot crypt
-	sed -i 's#root=/dev/mmcblk0p2#root=/dev/mapper/crypt cryptdevice=/dev/mmcblk0p2:crypt#g' ${_BASEDIR}/root/boot/cmdline.txt
+	sed -i 's#root=/dev/mmcblk0p2#root=/dev/mapper/crypt cryptdevice=/dev/mmcblk0p2:crypt#g' ${_BUILDDIR}/root/boot/cmdline.txt
 	# Enable cryptsetup when building initramfs
-	echo "CRYPTSETUP=y" >> ${_BASEDIR}/root/etc/cryptsetup-initramfs/conf-hook
+	echo "CRYPTSETUP=y" >> ${_BUILDDIR}/root/etc/cryptsetup-initramfs/conf-hook
 	# Update /etc/fstab
-	sed -i 's#/dev/mmcblk0p2#/dev/mapper/crypt#g' ${_BASEDIR}/root/etc/fstab
+	sed -i 's#/dev/mmcblk0p2#/dev/mapper/crypt#g' ${_BUILDDIR}/root/etc/fstab
 	# Update /etc/crypttab
-	echo "crypt /dev/mmcblk0p2 none luks" >> ${_BASEDIR}/root/etc/crypttab
+	echo "crypt /dev/mmcblk0p2 none luks" >> ${_BUILDDIR}/root/etc/crypttab
 	# Create a hook to include our crypttab in the initramfs
-	cat << "EOF" > ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-cryptsetup
+	cat << "EOF" > ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-cryptsetup
 #!/bin/sh
 if [ "$1" = "prereqs" ]; then exit 0; fi
 . /usr/share/initramfs-tools/hook-functions
 mkdir -p ${DESTDIR}/cryptroot || true
 cat /etc/crypttab >> ${DESTDIR}/cryptroot/crypttab
 EOF
-	chmod 755 ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-cryptsetup
+	chmod 755 ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-cryptsetup
 	# Disable autoresize
-	chroot ${_BASEDIR}/root systemctl disable rpiwiggle
-	rm ${_BASEDIR}/root/root/scripts/rpi-wiggle.sh
+	chroot ${_BUILDDIR}/root systemctl disable rpiwiggle
+	rm ${_BUILDDIR}/root/root/scripts/rpi-wiggle.sh
 
 	echo "...encryptpi call completed!"
 }
@@ -134,17 +134,17 @@ finalstuff(){
 	echo "Starting finalstuff..."
 
 	# Disable lightdm
-	chroot ${_BASEDIR}/root systemctl disable lightdm
+	chroot ${_BUILDDIR}/root systemctl disable lightdm
 
 	# Install other handy tools
-	chroot ${_BASEDIR}/root apt-get -y install telnet dsniff bettercap
+	chroot ${_BUILDDIR}/root apt-get -y install telnet dsniff bettercap
 
 	# Clean apt
-	#chroot ${_BASEDIR}/root apt-get -y purge manpages man-db
-	chroot ${_BASEDIR}/root apt-get clean
+	#chroot ${_BUILDDIR}/root apt-get -y purge manpages man-db
+	chroot ${_BUILDDIR}/root apt-get clean
 
 	# Finally, Create the initramfs
-	chroot ${_BASEDIR}/root mkinitramfs -o /boot/initramfs.gz -v `ls ${_BASEDIR}/root/lib/modules/ | grep 'v8+' | head -n 1`
+	chroot ${_BUILDDIR}/root mkinitramfs -o /boot/initramfs.gz -v `ls ${_BUILDDIR}/root/lib/modules/ | grep 'v8+' | head -n 1`
 
 	echo "...finalstuff call completed!"
 }
@@ -154,10 +154,10 @@ iodinepi(){
 	# Begin Iodine
 	echo "Attempting iodinepi..."
 
-	chroot ${_BASEDIR}/root apt-get -y install iodine
+	chroot ${_BUILDDIR}/root apt-get -y install iodine
 
 	# Create initramfs hook file for iodine
-	cat << 'EOF2' > ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-iodine
+	cat << 'EOF2' > ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-iodine
 #!/bin/sh
 if [ "$1" = "prereqs" ]; then exit 0; fi
 . /usr/share/initramfs-tools/hook-functions
@@ -184,13 +184,13 @@ EOF
 chmod 755 ${DESTDIR}/start_iodine
 exit 0
 EOF2
-	chmod 755 ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-iodine
+	chmod 755 ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-iodine
 	# Replace variables in iodine hook file
-	sed -i "s#IODINE_PASSWORD#${_IODINE_PASSWORD}#g" ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-iodine
-	sed -i "s#IODINE_DOMAIN#${_IODINE_DOMAIN}#g" ${_BASEDIR}/root/etc/initramfs-tools/hooks/zz-iodine
+	sed -i "s#IODINE_PASSWORD#${_IODINE_PASSWORD}#g" ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-iodine
+	sed -i "s#IODINE_DOMAIN#${_IODINE_DOMAIN}#g" ${_BUILDDIR}/root/etc/initramfs-tools/hooks/zz-iodine
 
 	# Create initramfs script file for iodine
-	cat << 'EOF' > ${_BASEDIR}/root/etc/initramfs-tools/scripts/init-premount/iodine
+	cat << 'EOF' > ${_BUILDDIR}/root/etc/initramfs-tools/scripts/init-premount/iodine
 #!/bin/sh
 if [ "$1" = "prereqs" ]; then exit 0; fi
 startIodine(){
@@ -199,25 +199,25 @@ startIodine(){
 startIodine &
 exit 0
 EOF
-	chmod 755 ${_BASEDIR}/root/etc/initramfs-tools/scripts/init-premount/iodine
+	chmod 755 ${_BUILDDIR}/root/etc/initramfs-tools/scripts/init-premount/iodine
 
 	# Create iodine startup script (not initramfs)
-	cat << EOF > ${_BASEDIR}/root/opt/iodine
+	cat << EOF > ${_BUILDDIR}/root/opt/iodine
 #!/bin/bash
 while true;do
 	iodine -f -r -I1 -L0 -P IODINE_PASSWORD IODINE_DOMAIN
 	sleep 60
 done
 EOF
-	chmod 755 ${_BASEDIR}/root/opt/iodine
-	sed -i "s#IODINE_PASSWORD#${_IODINE_PASSWORD}#g" ${_BASEDIR}/root/opt/iodine
-	sed -i "s#IODINE_DOMAIN#${_IODINE_DOMAIN}#g" ${_BASEDIR}/root/opt/iodine
-	cat << EOF > ${_BASEDIR}/root/crontab_setup
+	chmod 755 ${_BUILDDIR}/root/opt/iodine
+	sed -i "s#IODINE_PASSWORD#${_IODINE_PASSWORD}#g" ${_BUILDDIR}/root/opt/iodine
+	sed -i "s#IODINE_DOMAIN#${_IODINE_DOMAIN}#g" ${_BUILDDIR}/root/opt/iodine
+	cat << EOF > ${_BUILDDIR}/root/crontab_setup
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 @reboot /opt/iodine
 EOF
-	chroot ${_BASEDIR}/root crontab /crontab_setup
-	rm ${_BASEDIR}/root/crontab_setup
+	chroot ${_BUILDDIR}/root crontab /crontab_setup
+	rm ${_BUILDDIR}/root/crontab_setup
 
 	echo "...iodinepi call completed!"
 }
@@ -242,14 +242,15 @@ trap '' SIGINT SIGQUIT SIGTSTP
 . cryptmypi.conf
 
 #we need this to exist before anything that follows
-_BASEDIR=`pwd`/cryptmypi-build
-if [ -d ${_BASEDIR} ];then
-	echo "Working directory already exists: ${_BASEDIR}"
+_BASEDIR=`pwd`
+_BUILDDIR=${_BASEDIR}/cryptmypi-build
+if [ -d ${_BUILDDIR} ];then
+	echo "Working directory already exists: ${_BUILDDIR}"
 	echo "Exiting ..."
 	exit 1
 fi
-mkdir -p ${_BASEDIR}
-cd ${_BASEDIR}
+mkdir -p ${_BUILDDIR}
+cd ${_BUILDDIR}
 
 # Main logic - infinite loop
 while true
