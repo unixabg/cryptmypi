b735d9acf8153cfe3139ef25154cb9de5f885c8c
Richard Nelson <unixabg@gmail.com>
2020-10-08 14:45:48 -0500

    Renamed the setup-stage1.sh and associated variables work in 6aa918b3a75 to harmonize with hook name and purpose. i.e. stage1-otherscript.sh and 3200-stage1-otherscript.hook and _STAGE1_OTHERSCRIPT.

--- examples/pios-encrypted-basic/stage1-otherscript.sh
+++ examples/pios-encrypted-basic/stage1-otherscript.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 ###################################
-# Raspbian Pi OS setup-stage1.sh
+# Raspbian Pi OS stage1-otherscript.sh
 
 
-echo 'Modifying /boot/cmdline.txt.'
+echo 'Modifying /boot/cmdline.txt for encrypted boot.'
 sed -i.bak "s#quiet init=/usr/lib/raspi-config/init_resize.sh##g" /boot/cmdline.txt
-sed -i.bak "s#PARTUUID=2fed7fee-02#/dev/mmcblk0p2#g" /boot/cmdline.txt
-
+sed -i.bak "s#root=PARTUUID=2fed7fee-02#root=/dev/mapper/crypt cryptdevice=/dev/mmcblk0p2:crypt#g" /boot/cmdline.txt
 
 echo 'Appendgin initramfs information to /boot/cmdline.txt.'
 echo 'initramfs initramfs.gz followkernel' >>  /boot/config.txt
 
 
-echo 'Updating partition information in /etc/fstab'
+echo 'Updating partition information in /etc/fstab for enctyped boot.'
 sed -i.bak "s#PARTUUID=2fed7fee-0#/dev/mmcblk0p#g" /etc/fstab
-
+sed -i 's#/dev/mmcblk0p2#/dev/mapper/crypt#g' /etc/fstab
 
 echo 'Enabling ssh on boot'
 touch /boot/ssh
