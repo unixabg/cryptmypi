d7f4211daa020600b8d4e0bfdd0318c081dcd623
Richard Nelson <unixabg@gmail.com>
2020-10-25 17:38:03 -0500

    Update pios-unencrypted example.

--- examples/pios-unencrypted/stage1-otherscript.sh
+++ examples/pios-unencrypted/stage1-otherscript.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 ###################################
 # Raspbian Pi OS stage1-otherscript.sh
@@ -6,15 +6,15 @@
 
 echo 'Modifying /boot/cmdline.txt.'
 sed -i.bak "s#quiet init=/usr/lib/raspi-config/init_resize.sh##g" /boot/cmdline.txt
-sed -i.bak "s#PARTUUID=2fed7fee-02#/dev/mmcblk0p2#g" /boot/cmdline.txt
+sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /boot/cmdline.txt
 
 
-echo 'Appendgin initramfs information to /boot/cmdline.txt.'
-echo 'initramfs initramfs.gz followkernel' >>  /boot/config.txt
+#echo 'Appending initramfs information to /boot/cmdline.txt.'
+#echo 'initramfs initramfs.gz followkernel' >>  /boot/config.txt
 
 
 echo 'Updating partition information in /etc/fstab'
-sed -i.bak "s#PARTUUID=2fed7fee-0#/dev/mmcblk0p#g" /etc/fstab
+sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /etc/fstab
 
 
 echo 'Enabling ssh on boot'
