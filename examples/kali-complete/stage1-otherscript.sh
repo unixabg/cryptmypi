23ac95353c19657a1335b875d46259d02e2ccd41
Richard Nelson <unixabg@gmail.com>
2020-10-27 20:47:59 -0500

    Improve shabang on kali-complete stage#-otherscript.sh scripts.

--- examples/kali-complete/stage1-otherscript.sh
+++ examples/kali-complete/stage1-otherscript.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
 echo 'Example of stage1 machine specific setup script.'
--- examples/kali-complete/stage2-otherscript.sh
+++ examples/kali-complete/stage2-otherscript.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
 echo 'Example of stage2 machine specific setup script.'
