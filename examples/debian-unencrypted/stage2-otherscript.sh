0ef32969bada3ed0bf474ddd508272609f9cbf3f
Richard Nelson <unixabg@gmail.com>
2020-11-13 13:40:00 -0600

    Improve debain-unencrypted to use generated initramfs in examples/debian-unencrypted/stage2-otherscript.sh.

--- examples/debian-unencrypted/stage2-otherscript.sh
+++ examples/debian-unencrypted/stage2-otherscript.sh
@@ -9,3 +9,9 @@ echo 'Setting up partition labels for Debian OS.'
 dosfslabel /dev/sdb1 RASPIFIRM
 e2label /dev/sdb2 RASPIROOT
 
+
+# Move our kernel in place of the targets default kernel
+__DEBIAN_KERNEL="initrd.img-5.8.0-0.bpo.2-arm64"
+echo "Movinng our /boot/initramfs.gz to /boot/${__DEBIAN_KERNEL}."
+mv "/boot/${__DEBIAN_KERNEL}" "/boot/${__DEBIAN_KERNEL}-oos"
+mv /boot/initramfs.gz "/boot/${__DEBIAN_KERNEL}"
