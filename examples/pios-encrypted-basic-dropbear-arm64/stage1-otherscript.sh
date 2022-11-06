00b439d4d06ef1a96377c09c6107f3eeed0d5ed3
Richard Nelson <unixabg@gmail.com>
2022-11-06 07:51:42 -0600

    Update examples/pios-*/stange1-otherscript.sh.

--- examples/pios-encrypted-basic-dropbear-arm64/stage1-otherscript.sh
+++ examples/pios-encrypted-basic-dropbear-arm64/stage1-otherscript.sh
@@ -5,9 +5,7 @@
 
 
 echo 'Modifying /boot/cmdline.txt for encrypted boot.'
-sed -i.bak "s#quiet init=/usr/lib/raspi-config/init_resize.sh##g" /boot/cmdline.txt
-sed -i.bak "s#root=PARTUUID=.*-02#root=/dev/mapper/crypt#g" /boot/cmdline.txt
-
+sed -i.bak -e "s#quiet init=/usr/lib/raspberrypi-sys-mods/firstboot##g" -e "s#root=PARTUUID=.*-02#root=/dev/mapper/crypt#g" /boot/cmdline.tx
 
 echo 'Updating partition information in /etc/fstab for enctyped boot.'
 sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /etc/fstab
@@ -20,3 +18,7 @@ rm -f /usr/lib/systemd/system/userconfig.service
 
 echo 'Enabling ssh on boot'
 touch /boot/ssh
+
+
+# Disable user creation prompt
+systemctl disable userconfig > /dev/null 2&>1
--- examples/pios-unencrypted/stage1-otherscript.sh
+++ examples/pios-unencrypted/stage1-otherscript.sh
@@ -5,9 +5,7 @@
 
 
 echo 'Modifying /boot/cmdline.txt.'
-sed -i.bak "s#quiet init=/usr/lib/raspi-config/init_resize.sh##g" /boot/cmdline.txt
-sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /boot/cmdline.txt
-
+sed -i.bak -e "s#quiet init=/usr/lib/raspberrypi-sys-mods/firstboot##g" -e "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /boot/cmdline.txt
 
 #echo 'Appending initramfs information to /boot/cmdline.txt.'
 #echo 'initramfs initramfs.gz followkernel' >>  /boot/config.txt
@@ -19,3 +17,7 @@ sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /etc/fstab
 
 echo 'Enabling ssh on boot'
 touch /boot/ssh
+
+
+# Disable user creation prompt
+systemctl disable userconfig > /dev/null 2&>1
