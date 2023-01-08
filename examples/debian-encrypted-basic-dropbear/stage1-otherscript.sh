25fdffe20ed2f0f63ac0e3cf4f8778279305a05a
Andras Gemes <andrasgemes@outlook.com>
2023-01-08 15:14:56 +0100

    Fix typos in stage1-otherscript.sh files.

--- examples/debian-encrypted-basic-dropbear/stage1-otherscript.sh
+++ examples/debian-encrypted-basic-dropbear/stage1-otherscript.sh
@@ -4,5 +4,5 @@
 # Debian encrypted stage1-otherscript.sh
 
 
-echo 'Updating partition information in /etc/fstab for enctyped boot.'
+echo 'Updating partition information in /etc/fstab for encrypted boot.'
 sed -i 's#LABEL=RASPIROOT#/dev/mapper/crypt#g' /etc/fstab
--- examples/pios-encrypted-basic-dropbear-arm64/stage1-otherscript.sh
+++ examples/pios-encrypted-basic-dropbear-arm64/stage1-otherscript.sh
@@ -7,7 +7,7 @@
 echo 'Modifying /boot/cmdline.txt for encrypted boot.'
 sed -i.bak -e "s#quiet init=/usr/lib/raspberrypi-sys-mods/firstboot##g" -e "s#root=PARTUUID=.*-02#root=/dev/mapper/crypt#g" /boot/cmdline.txt
 
-echo 'Updating partition information in /etc/fstab for enctyped boot.'
+echo 'Updating partition information in /etc/fstab for encrypted boot.'
 sed -i.bak "s#PARTUUID=.*-0#/dev/mmcblk0p#g" /etc/fstab
 sed -i 's#/dev/mmcblk0p2#/dev/mapper/crypt#g' /etc/fstab
 
@@ -21,4 +21,4 @@ touch /boot/ssh
 
 
 # Disable user creation prompt
-systemctl disable userconfig > /dev/null 2&>1
+systemctl disable userconfig > /dev/null 2>&1
